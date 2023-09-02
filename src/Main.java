650491e7cecf00cc701f4b26f2ca96c243def4e5
Sagida07 <sagidatentimishova@gmail.com>
2023-09-02 21:19:40 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String mySchoolNumber;
+
+        final int NUM = 29;
+
+        String word = " school";
+
+        mySchoolNumber = NUM + word;
+        System.out.println(mySchoolNumber);
+
+
+        if (NUM > 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        } else if (NUM < 0) {
+            System.out.println("Вы сохранили положительное число");
+        } else {
+            System.out.println("Вы сохранили 0");
+        }
+
+        System.out.println("Введите ваше имя: ");
+        Scanner run = new Scanner(System.in);
+        String name = run.nextLine();
+        System.out.println("Привет, " + name + "!");
+    }
+}
